8a88e57addd603298d349d5e7758f002933cc15d
Yakob <126866800+Yakob-D@users.noreply.github.com>
2024-12-09 16:09:50 +0300

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
-monthly_savings = income - expenses
-projected_savings = savings * 12 + int((savings * 12 * 0.05))
-print("Your monthly savings are $" + str(savings))
-print("Projected savings after one year, with interest, is: $" + str(annual_savings))
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expense = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = monthly_savings * 12 + int((monthly_savings * 12 * 0.05))
+print("Your monthly savings are $" + str(monthly_savings))
+print("Projected savings after one year, with interest, is: $" + str(projected_savings))
