0e0a71de40604e134a52f3daffce2179f8b00b95
Yakob <126866800+Yakob-D@users.noreply.github.com>
2025-01-17 12:35:42 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 class BankAccount:
     def __init__(self, account_balance, initial_balance):
         self.account_balance = account_balance
-        self.initial_balance = initial_balance
+        self.initial_balance = 0
     
     def deposit(self, amount):
         self.account_balance += amount
